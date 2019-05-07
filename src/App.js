2605d84873344724049415659fb008ec54b3884e
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-05-07 01:27:07 -0400

    Basic routing in place

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+import "./materialize.css";
+import "./style.css";
+
+import Home from "./components/home";
+
+function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <nav class="red accent-4" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="/home" class="brand-logo"><i class="fas fa-bicycle"></i></a>
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/survey">Survey</Link>
+            </li>
+            <li>
+              <Link to="/buddies">Buddies</Link>
+            </li>
+            <li>
+              <Link to="/messages">Messages</Link>
+            </li>
+            <li>
+              <Link to="/messages">Settings</Link>
+            </li>  
+          </ul>
+        </div>
+      </nav>
+      <div>
+        <a href="https://github.com/olen-d/veloMatchr">Github Repo</a>
+      </div>
+
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+    </Router>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
     </div>
   );
 }
 
-export default App;
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Topic({ match }) {
+  return (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
+}
+
+export default BasicExample;
+
---
+++ src/components/home.js
@@ -0,0 +1,86 @@
+import React from "react"
+
+const Home = () => {
+    return(
+        <>
+            <div class="container">
+                <br />
+                <br />
+                <h1 class="header center orange-text text-darken-2">
+                    VeloMatchr
+                </h1>
+                <div class="row center">
+                    <h5 class="header col s12 light">
+                        Find the riding buddy of your dreams.
+                    </h5>
+                </div>
+                <div class="row">
+                    <div class="col s12 m6 center">
+                        <a href="survey" id="survey-button" class="btn-large waves-effect waves-light red accent-4"><i class="fas fa-list-alt"></i> Get Started</a>
+                    </div>
+                    <div class="col s12 m6">
+                        <form id="login">
+                        <div class="input-field col s12 m10 l8">
+                            <input id="your_email" type="text" class="validate" required />
+                            <label for="your_email">Your Email Address (Required)</label>
+                        </div>
+                        <div class="input-field col s12 m10 l8">
+                            <input id="your_password" type="password" class="validate" required />
+                            <label for="your_password">Your Password (Required)</label>
+                        </div>
+                        </form>
+                    </div>
+                </div>      
+            </div>
+            <div class="container">
+                <div class="section">
+                    <div class="row">
+                        <div class="col s12 m4">
+                            <div class="icon-block">
+                                <h2 class="center red-text text-darken-3">
+                                    <i class="material-icons">flash_on</i>
+                                </h2>
+                                <h5 class="center">
+                                    Find a riding buddy fast
+                                </h5>
+                                <p class="light">
+                                    Our ten question survey designed to be filled out in five minutes or less, so you can quickly find a buddy and ride. We refined our survey through rigourous testing to identify questions with the highest information density to save you time while providing the highest quality matches.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="icon-block">
+                                <h2 class="center red-text text-darken-3">
+                                    <i class="material-icons">group</i>
+                                </h2>
+                                <h5 class="center">
+                                    Compatibility focused
+                                </h5>
+                                <p class="light">
+                                    No one wants to ride with the person who is always surging when it's their turn at the front of the paceline. Or that person who natters on incessantly about the latest gear. Our proprietary algorithm uses a unique framework to provide you with the most compatible riding buddy.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="icon-block">
+                                <h2 class="center red-text text-darken-3">
+                                    <i class="material-icons">settings</i>
+                                </h2>
+                                <h5 class="center">
+                                    Easy to use
+                                </h5>
+                                <p class="light">
+                                    We kept VeloMatchr simple and intuitive so that you can focus on what's important - riding with a buddy. Just click the "Get Started" button, fill out the survey, and get a riding buddy.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <br />
+            </div>
+        </>
+    );
+}
+
+export default Home;
