37173e11f1f857eff3ff6272ab3944d360b913e7
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-08-27 23:25:06 -0400

    Added matching algorithms

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
 
   const bcrypt = require("./../helpers/bcrypt-module");
 
+  const Sequelize = require("sequelize");
   const db = require("./../models");
 
   // Set Storage
@@ -88,6 +89,49 @@ module.exports = (app) => {
       userId: userId,
       answers: answers.join()
     }).then(newAnswer => {
+      // Run the matching algorithm here, rather than in the survey component so the
+      // survey form can be re-used for any type of likert scale survey
+
+      // Retrieve userid and answers from the data
+      const Op = Sequelize.Op;
+
+      db.Answer.findAll({
+        where: {
+          [Op.not]: [{id: newAnswer.id}]
+        },
+        attributes: ["userId", "answers"]
+      }).then(otherAnswers => {
+        let scores = new Map();
+
+        const newAnswers = newAnswer.answers.split(",");
+        // console.log("||||| newAnswers: ", newAnswers);
+        // console.log("---------\nOther Answers:\n", otherAnswers);
+        for (let a of otherAnswers) {
+          let thisAnswers = a.answers.split(",");
+          let diffs = newAnswers.map((w, i) => {
+            let r = Math.abs(w - thisAnswers[i]);
+            console.log(thisAnswers[i]);
+            return r;
+          });
+
+          // Calculate the total difference in scores
+          let score = diffs.reduce((a, c) => {
+            let s = a + c;
+            return s;
+          });
+        
+          scores.set(a.userId, score);
+          // console.log("/// Testing: ", a.userId, " ", a.answers);
+        }
+        console.log("-------------\nScores: ", scores);
+      });
+
+      // Should be data.answers
+      // console.log("Answers\n", data.answers);
+      // console.log("API Answers \n",newAnswer.answers);
+      // Run the matching algorithm
+      // Redirect to the matches page
+      // Remember to finish the matches page
       return res.json(newAnswer);
     }).catch(error => {
       res.send({
