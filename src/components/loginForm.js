a8e5cc86db400f0d548a7dc8f5087652099ecaa1
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-12-18 00:47:05 -0500

    Authorization added to Context

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
 import { 
   Link,
   Redirect
 } from "react-router-dom";
+
+import { useAuth } from "../context/authContext";
 
 import {
   Button,
@@ -14,24 +16,16 @@ import {
   Segment
 } from "semantic-ui-react"
 
-class LoginForm extends Component {
-  state = {
-    username: "",
-    pass: "",
-    toDashboard: false
-  }
+const LoginForm = props => {
+  // Set up the state
+  const [toDashboard, setToDashboard] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [username, setUsername] = useState("");
+  const [pass, setPass] = useState("");
 
-  onChange = e => {
-    this.setState({ [e.target.name] : e.target.value });
-  }
+  const { setAuthTokens } = useAuth();
 
-  onSubmit = e => {
-    e.preventDefault();
-
-    const {
-      username,
-      pass
-    } = this.state;
+  const postLogin = () => {
 
     const formData = { 
       username,
@@ -46,94 +40,90 @@ class LoginForm extends Component {
       body: JSON.stringify(formData)
     }).then(response => {
       return response.json();
-    }).then(data => { console.log("DATA\n",data)
+    }).then(data => {
       if(data.token) {
-        localStorage.setItem("user_token", data.token);
-        this.setState({ userToken: data.token, authenticated: data.authenticated, toDashboard: true });
-        window.location.reload();
+        setAuthTokens(data.token);
+        setToDashboard("ChainsawDeath");
       } else {
-        localStorage.removeItem("user_token");
+        // localStorage.removeItem("user_token");
+        setIsError(true);
       }
     }).catch(error => {
-      return ({
-        errorCode: 500,
-        errorMsg: "Internal Server Error",
-        errorDetail: error
-      })
+        setIsError(true);
     });
   }
 
-  render() {
-    if (this.state.toDashboard === true)
-      {
-        return <Redirect to="/home" />
-      }
-    const {
-      username,
-      pass
-    } = this.state;
-
-    return(
-      <Grid.Column width={this.props.colWidth}>
-        <Header 
-          as="h2" 
-          textAlign="center"
-          color="grey"
-        >
-          {this.props.formTitle}
-        </Header>
-        <Segment>
-          <Form 
-            size="large"
-            onSubmit={this.onSubmit}
-          >
-            <Form.Input
-              fluid
-              icon="envelope"
-              iconPosition="left"
-              name="username"
-              value={username}
-              placeholder="Your Email Address"
-              onChange={this.onChange}
-            />
-            <Form.Input
-              fluid
-              icon="lock"
-              iconPosition="left"
-              name="pass"
-              value={pass}
-              placeholder="Your Password"
-              type="password"
-              onChange={this.onChange}
-            />
-            <Button
-              fluid
-              type="submit" 
-              color="red"
-              size="large"
-            >
-              Sign In
-            </Button>
-          </Form>
-        </Segment>
-        <Message>
-          <p>
-            Don't have an account yet?
-          </p>
-          <Link to="/survey">
-            <Button
-              color="red"
-              size="large"
-              content="Get Started"
-              icon="list alternate"
-              labelPosition="left"
-            >
-            </Button>
-          </Link>
-        </Message>
-      </Grid.Column>
-    );
+  if(toDashboard) {
+    return <Redirect to="/" />;
   }
+
+  // And now we make the JSX...
+  return(
+    <Grid.Column width={props.colWidth}>
+      <Header 
+        as="h2" 
+        textAlign="center"
+        color="grey"
+      >
+        {props.formTitle}
+      </Header>
+      <Segment>
+        <Form 
+          size="large"
+          // onSubmit={this.onSubmit}
+        >
+          <Form.Input
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            name="username"
+            value={username}
+            placeholder="Your Email Address"
+            onChange={e => {
+              setUsername(e.target.value);
+            }}
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            name="pass"
+            value={pass}
+            placeholder="Your Password"
+            type="password"
+            onChange={e => {
+              setPass(e.target.value);
+            }}
+          />
+          <Button 
+            fluid
+            type="submit"
+            color="red"
+            size="large"
+            onClick={postLogin}
+          >
+            Sign In
+          </Button>
+        </Form>
+      </Segment>
+      <Message>
+        <p>
+          Don't have an account yet?
+        </p>
+        <Link to="/survey">
+          <Button
+            color="red"
+            size="large"
+            content="Get Started"
+            icon="list alternate"
+            labelPosition="left"
+          >
+          </Button>
+        </Link>
+      </Message>
+    </Grid.Column>
+  );
 }
+
 
 export default LoginForm;
