a465cf33e55de29ad76ea9cd8b721fb23025a670
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-02-01 17:57:57 -0500

    Interim save

--- app/controllers/surveyController.js
+++ app/controllers/surveyController.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 const Sequelize = require("sequelize");
 
 // Models
@@ -42,7 +43,23 @@ exports.read_survey_response = (req, res) => {
 exports.read_survey_response_except = (req, res) => {
   const userId = req.params.userid;
 
-  // Need the match preference of the user
+  fetch(`${process.env.REACT_APP_API_URL}/api/users/matches/preferences/${userId}`)
+  .then(response => {
+    return response.ok ? response.json() : new Error(response.statusText); 
+  })
+  .then(json => {
+    const { user: { gender, userMatchPrefs: { distance, gender: matchGenderPref }, }, } = json; // Nested destructuring. Returns distance and gender. Pretty dope.
+
+    console.log("DISTANCE:\n", distance, "\nGENDER PREF\n", matchGenderPref, "\nuserGender\n", gender);
+    // if(gender === "any") {
+    //   const whereClause = `[Op.or]: [{"$matchPrefs.gender$": "any"}, {"$matchPrefs.gender$": "same". }]`;
+    //   readAnswersByPrefs(distance, gender);
+    // }
+  })
+  .catch(err => {
+    // TODO: do something about the error
+    console.log("surveyController.js ~59 - Error:", err);
+  })
 
   // Match preference of user is same
   // Need gender of user
@@ -51,31 +68,34 @@ exports.read_survey_response_except = (req, res) => {
 
   
   // Match preference of user is any
-  // Match other users with preference of any
+  // Match other users with preference of any or preference of same && user gender === match gender
 
   // Need the match preference of the potential matches
+  const readAnswersByPrefs = (distance, gender) => {
+    const genderMatch = gender;
 
-  Answer.findAll({
-    where: {
-      [Op.not]: [{userId}]
-    },
-    attributes: ["userId", "answers"],
-    include: [
-      {
-        model: User,
-        as: "matchCharacteristics",
-        attributes: ["gender", "latitude", "longitude"]
+    Answer.findAll({
+      where: {
+        [Op.not]: [{userId}]
       },
-      {
-        model: MatchPref,
-        as: "matchPrefs",
-        attributes: ["distance", "gender"]
-      }
-    ]
-  }).then(otherAnswers => {
-    res.json(otherAnswers);
-  })
-  .catch(err => {
-    res.status(500).json({error: err});
-  });
+      attributes: ["userId", "answers"],
+      include: [
+        {
+          model: User,
+          as: "matchCharacteristics",
+          attributes: ["gender", "latitude", "longitude"]
+        },
+        {
+          model: MatchPref,
+          as: "matchPrefs",
+          attributes: ["distance", "gender"]
+        }
+      ]
+    }).then(otherAnswers => {
+      res.json(otherAnswers);
+    })
+    .catch(err => {
+      res.status(500).json({error: err});
+    });
+  }
 };
