73c513841b7fa94f9606692429d3a729de22f561
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-02-16 20:39:18 -0500

    Converted Home component to functional component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 
 import {
   Container,
@@ -8,118 +8,95 @@ import {
 
 import SignupRequiredForm from "../components/signupRequiredForm";
 
-class Home extends Component {
-  state = {
-    data: null
-  };
-
-  // componentDidMount() {
-  //     // Call our fetch function below once the component mounts
-  //   this.callBackendAPI()
-  //     .then(res => this.setState({ data: res.express }))
-  //     .catch(err => console.log(err));
-  // }
-  //   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  // callBackendAPI = async () => {
-  //   const response = await fetch('http://localhost:5000/api/express_backend');
-  //   const body = await response.json();
-
-  //   if (response.status !== 200) {
-  //     throw Error(body.message) 
-  //   }
-  //   return body;
-  // };
-
-  render() {
-    return(
-      <Container>
-        <Grid>
-          <Grid.Column width={16} >
-            <Header
-              as="h1" 
-              textAlign="center"
-              color="orange"
-            >
-                VeloMatchr
-            </Header>
-            <Header
-              as="h2"
-              textAlign="center" 
-              color="grey"
-            >
-              Find the riding buddy of your dreams.
-            </Header>
-            {/* <p>Status: {this.state.data}</p> */}
-          </Grid.Column>
-        </Grid>
-        <Grid stackable>
-          <SignupRequiredForm
-            colWidth="6"
-            formTitle="Sign Up Today"
-          />
-        </Grid>          
-        <Grid stackable columns="equal">
-          <Grid.Column>
-            <Header
-              as="h1" 
-              textAlign="center"
-              color="red"
-            >
-              <i className="bolt icon"></i>
-            </Header>
-            <Header
-              as="h3"
-              textAlign="center"
-              color="grey" className="ui header grey"
-            >
-              Find a riding buddy fast
-            </Header>
-            <p className="light">
-              Our ten question survey is designed to be filled out in five minutes or less, so you can quickly find a buddy and ride. We refined our survey through rigourous testing to identify questions with the highest information density to save you time while providing the highest quality matches.
-            </p>
-          </Grid.Column>
-          <Grid.Column>
-            <Header
-              as="h1" 
-              textAlign="center"
-              color="red"
-            >
-              <i className="users icon"></i>
-            </Header>
-            <Header
-              as="h3"
-              textAlign="center"
-              color="grey" className="ui header grey"
-            >
-              Compatibility focused
-            </Header>
-            <p className="light">
-              No one wants to ride with the person who is always surging when it's their turn at the front of the paceline. Or that person who natters on incessantly about the latest gear. Our proprietary algorithm uses a unique framework to provide you with the most compatible riding buddy.
-            </p>
-          </Grid.Column>
-          <Grid.Column>
-            <Header
-              as="h1" 
-              textAlign="center"
-              color="red"
-            >
-              <i className="setting icon"></i>
-            </Header>
-            <Header
-              as="h3"
-              textAlign="center"
-              color="grey" className="ui header grey"
-            >
-              Easy to use
-            </Header>
-            <p className="light">
-              We kept VeloMatchr simple and intuitive so that you can focus on what's important - riding with a buddy. Just click the "Get Started" button, fill out the survey, and get matched with a riding buddy.
-            </p>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    );
-  }
+const Home = () => {
+  return(
+    <Container>
+      <Grid>
+        <Grid.Column width={16} >
+          <Header
+            as="h1" 
+            textAlign="center"
+            color="orange"
+          >
+              VeloMatchr
+          </Header>
+          <Header
+            as="h2"
+            textAlign="center" 
+            color="grey"
+          >
+            Find the riding buddy of your dreams.
+          </Header>
+          {/* <p>Status: {this.state.data}</p> */}
+        </Grid.Column>
+      </Grid>
+      <Grid stackable>
+        <SignupRequiredForm
+          colWidth="6"
+          formTitle="Sign Up Today"
+        />
+      </Grid>          
+      <Grid stackable columns="equal">
+        <Grid.Column>
+          <Header
+            as="h1" 
+            textAlign="center"
+            color="red"
+          >
+            <i className="bolt icon"></i>
+          </Header>
+          <Header
+            as="h3"
+            textAlign="center"
+            color="grey" className="ui header grey"
+          >
+            Find a riding buddy fast
+          </Header>
+          <p className="light">
+            Our ten question survey is designed to be filled out in five minutes or less, so you can quickly find a buddy and ride. We refined our survey through rigourous testing to identify questions with the highest information density to save you time while providing the highest quality matches.
+          </p>
+        </Grid.Column>
+        <Grid.Column>
+          <Header
+            as="h1" 
+            textAlign="center"
+            color="red"
+          >
+            <i className="users icon"></i>
+          </Header>
+          <Header
+            as="h3"
+            textAlign="center"
+            color="grey" className="ui header grey"
+          >
+            Compatibility focused
+          </Header>
+          <p className="light">
+            No one wants to ride with the person who is always surging when it's their turn at the front of the paceline. Or that person who natters on incessantly about the latest gear. Our proprietary algorithm uses a unique framework to provide you with the most compatible riding buddy.
+          </p>
+        </Grid.Column>
+        <Grid.Column>
+          <Header
+            as="h1" 
+            textAlign="center"
+            color="red"
+          >
+            <i className="setting icon"></i>
+          </Header>
+          <Header
+            as="h3"
+            textAlign="center"
+            color="grey" className="ui header grey"
+          >
+            Easy to use
+          </Header>
+          <p className="light">
+            We kept VeloMatchr simple and intuitive so that you can focus on what's important - riding with a buddy. Just click the "Get Started" button, fill out the survey, and get matched with a riding buddy.
+          </p>
+        </Grid.Column>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Home;
