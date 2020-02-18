dbadac0f6f18a260ee188f55cdd1fbd9d72f4704
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-02-18 00:36:43 -0500

    Added route to get userid by email address

--- app/routes/users.js
+++ app/routes/users.js
@@ -12,11 +12,13 @@ router.post(
   );
 
 router.get("/users/:username", users_controller.read_one_user);
+router.get("/users/email/:email", users_controller.read_one_user_id_by_email);
 router.get("/users/id/:userId", users_controller.read_one_user_by_id);
 router.get("/users/matches/preferences/:userId", users_controller.read_one_user_and_matches_preferences);
 
 router.post("/users/login", users_controller.read_login);
 router.post("/users/email/verify", users_controller.read_one_email_verification);
+router.post("/users/password/reset", users_controller.reset_user_password);
 
 router.put("/users/profile/required/update", users_controller.update_profile_required);
 router.put("/users/verified/update", users_controller.update_is_email_verified);
