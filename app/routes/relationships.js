3800ebc282781026dec29099598ad8317e0d63a5
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-07-27 00:04:57 -0400

    Removed deprecated body-parser from Relationships Routes

--- app/routes/relationships.js
+++ app/routes/relationships.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const app = express();
 
 const router = express.Router();
-
-const bodyParser = require("body-parser");
-app.use(bodyParser);
 
 const authorizeJWT = require("../helpers/authorize-jwt");
 
@@ -12,11 +8,11 @@ const relationships_controller = require("../controllers/relationshipsController
 
 router.post("/relationships/create", relationships_controller.update_user_relationships);
 
-router.get("/relationships/user/id/:userid", [bodyParser(), authorizeJWT], relationships_controller.read_user_relationships_by_id);
-router.get("/relationships/matched/count/user/id/:userid", [bodyParser(), authorizeJWT], relationships_controller.read_user_matched_count_by_id);
+router.get("/relationships/user/id/:userid", authorizeJWT, relationships_controller.read_user_relationships_by_id);
+router.get("/relationships/matched/count/user/id/:userid", authorizeJWT, relationships_controller.read_user_matched_count_by_id);
 
 router.put("/relationships/status/update", relationships_controller.update_user_relationship_status);
 
-router.delete("/relationships/delete/requester/id/:userid", [bodyParser(), authorizeJWT], relationships_controller.delete_user_relationships);
+router.delete("/relationships/delete/requester/id/:userid", authorizeJWT, relationships_controller.delete_user_relationships);
 
 module.exports = router;
