aa7af538fe001f5e195c6f26de8db18d747712eb
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-08-21 00:19:34 -0400

    Added authentication using jwt and included Auth headers in route to get survey answers for all users other than the current user for api/survey/except/id/{userID}

--- app/routes/survey.js
+++ app/routes/survey.js
@@ -9,6 +9,6 @@ const survey_controller = require("../controllers/surveyController");
 router.post("/survey/submit", authorizeJWT, survey_controller.update_survey_response);
 
 router.get("/survey/user/id/:userid", authorizeJWT, survey_controller.read_survey_response_by_id);
-router.get("/survey/except/:userid", survey_controller.read_survey_response_except);
+router.get("/survey/except/id/:userid", authorizeJWT, survey_controller.read_survey_response_except);
 
 module.exports = router;
