e79aabd324ae7d90603e8b40d0253f4613bec464
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-09-09 00:54:02 -0400

    Sender email proxy added to compose email form

--- src/components/composeEmailForm.js
+++ src/components/composeEmailForm.js
@@ -1,5 +1,11 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 // import PropTypes from "prop-types";
+
+import {
+  useParams
+} from "react-router-dom";
+
+import auth from "./auth";
 
 import {
     Button,
@@ -10,7 +16,7 @@ import {
     Segment
   } from "semantic-ui-react";
   
-  import { AuthContext } from "../context/authContext";
+  import { useAuth } from "../context/authContext";
 
   import BodyTextarea from "./formFields/bodyTextarea";
   import ErrorContainer from "./errorContainer";
@@ -21,12 +27,54 @@ import {
 const ComposeEmailForm = props => {
   const { colWidth, formTitle } = props;
 
+  const [addresseeId, setAddresseeId] = useState(null);
+  const [addresseeProxy, setAddresseeProxy] = useState(null);
   const [flag, setFlag] = useState(true);
+  const [requesterProxy, setRequesterProxy] = useState(null);
+  const [userId, setUserId] = useState(null);
 
+  const { id } = useParams();
+
+  const { authTokens: token } = useAuth();
+
+  const { user } = auth.getUserInfo(token);
 
   const initialValues = { body: "", subject: "Let's Ride Together" }
 
   const { errors, handleBlur, handleChange, handleServerErrors, initializeFields, values } = useForm();
+
+  useEffect(() => { setAddresseeId(id); }, [id]);
+  useEffect(() => { setUserId(user); }, [user]);
+
+  // Get email proxies
+  useEffect(() => {
+    if (userId && addresseeId) {
+      (async () => {
+        try {
+          const response = await fetch(`${process.env.REACT_APP_API_URL}/api/relationships/email-proxy/requester/id/${userId}/${addresseeId}`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+  
+          const json = response.ok ? await response.json() : setRequesterProxy("error");
+          const { data: [{ emailProxy }], } = json;
+
+          if (!emailProxy) {
+            // Need to create proxy
+            // setRequesterProxy
+            // setAddresseeProxy
+          } else {
+            setRequesterProxy(emailProxy);
+            // Now get the addressee proxy...
+          }
+        } catch(error) {
+          // TODO: deal with the error
+        }
+      })()
+    }
+  }, [addresseeId, token, userId]);
+
   
   if (flag) {
     initializeFields(initialValues);
@@ -59,7 +107,7 @@ const ComposeEmailForm = props => {
           handleChange={handleChange}
           values={values}
         />
-      </Form>
+      </Form>{userId} <br />{requesterProxy}
     </Grid.Column>
   );
 }
