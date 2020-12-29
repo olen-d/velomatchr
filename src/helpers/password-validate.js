b94d3f6afe0b84aa181c930b93800e1300441310
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-12-28 23:51:56 -0500

    Fixed issue where a rejected promise was uncaught in useForm when awaiting validation on blur

--- src/helpers/password-validate.js
+++ src/helpers/password-validate.js
@@ -1,6 +1,12 @@
-// Functions
+/**
+ * Validate that a potential password meets minimum requirements
+ * @author Olen Daelhousen <matchr@olen.dev>
+ * @param {string} password - the password to be tested
+ * @returns {Promise} Promise object represents true if the password passes validation or false if not
+ */
+
 const validatePassword = password => {
-    return new Promise((res, rej) => {
+    return new Promise((resolve, reject) => {
       try {
         const oneUpper = /[A-Z]/
         const oneLower = /[a-z]/
@@ -11,15 +17,15 @@ const validatePassword = password => {
         const isOneLower = oneLower.test(password);
         const isOneDigit = oneDigit.test(password);
         const isOneSpecial = oneSpecial.test(password);
-        const isLength = password.length >= 8;
+        const isLength = password && password.length >= 8; // Short circuit to avoid promise rejection if null or undefined password is passed
 
         if (isOneUpper && isOneLower && (isOneDigit || isOneSpecial) && isLength) {
-          res(true);
+          resolve(true);
         } else {
-          res(false);
+          resolve(false);
         }
       } catch {
-        rej(false);
+        reject(false);
       }
     });
   };
