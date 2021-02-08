5dea942cbfc13cba112421c0fd0faf9746ebf804
Salvador Dieffenbach <zogwarg@hotmail.com>
2021-02-07 21:54:07 -0500

    Add authorizeJWT middleware to relationships routes without it

--- app/routes/relationships.js
+++ app/routes/relationships.js
@@ -7,10 +7,10 @@ const authorizeJWT = require("../helpers/authorize-jwt");
 const relationships_controller = require("../controllers/relationshipsController");
 //TODO - Remove CRUD references from the routes (i.e. relationships/create, /relationships/status/update, relationships/delete)
 router.post("/relationships/create", authorizeJWT, relationships_controller.update_user_relationships);
-router.post("/relationships/email-proxy", relationships_controller.create_email_proxy);
+router.post("/relationships/email-proxy", authorizeJWT, relationships_controller.create_email_proxy);
 
-router.get("/relationships/email-address/:proxy", relationships_controller.read_email_address_by_proxy);
-router.get("/relationships/email-proxy/id/:requesterid/:addresseeid", relationships_controller.read_email_proxy_by_id);
+router.get("/relationships/email-address/:proxy", authorizeJWT, relationships_controller.read_email_address_by_proxy);
+router.get("/relationships/email-proxy/id/:requesterid/:addresseeid", authorizeJWT, relationships_controller.read_email_proxy_by_id);
 router.get("/relationships/matched/count/user/id/:userid", authorizeJWT, relationships_controller.read_user_matched_count_by_id);
 router.get("/relationships/status/ids", authorizeJWT, relationships_controller.read_relationship_status_by_id);
 router.get("/relationships/user/id/:userid", authorizeJWT, relationships_controller.read_user_relationships_by_id);
