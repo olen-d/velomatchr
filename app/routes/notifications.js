aebb668d521f166dc87cebe23e2dc8b65065a189
Salvador Dieffenbach <zogwarg@hotmail.com>
2021-02-23 23:58:51 -0500

    Add authentication to notifications routes

--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -6,8 +6,8 @@ const authorizeJWT = require("../helpers/authorize-jwt");
 
 const notifications_controller = require("../controllers/notificationsController");
 
-router.post("/notifications/preferences", notifications_controller.create_notification_preferences); // TODO: add authorizeJWT
+router.post("/notifications/preferences", authorizeJWT, notifications_controller.create_notification_preferences);
 
-router.put("/notifications/preferences", notifications_controller.update_notification_preferences); // TODO: add authorizeJWT
+router.put("/notifications/preferences", authorizeJWT, notifications_controller.update_notification_preferences);
 
 module.exports = router;
