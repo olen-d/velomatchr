c548d7e2ae2c543bc59532100e054c30062ca1c6
olen-d <olen.daelhousen@outlook.com>
2021-04-05 00:26:15 -0400

    Refactor Matches page to create standalone Matches List components

---
+++ src/components/matchLists.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import { MatchesContext } from "../context/matchesContext";
+
+import { Grid } from "semantic-ui-react";
+
+import MatchesList from "./../components/matchesList";
+
+const MatchLists = () => {
+  const [matches, setMatches] = useState({ error: null, matchesResult: [], isLoading: false });
+  const [totalPotential, setTotalPotential] = useState(0);
+  const [totalRequested, setTotalRequested] = useState(0);
+  const [totalMatched, setTotalMatched] = useState(0);
+
+  return(
+    <MatchesContext.Provider value={{matches, setMatches, totalPotential, setTotalPotential, totalRequested, setTotalRequested, totalMatched, setTotalMatched}}>
+      <Grid.Row>
+        <Grid.Column width={4}>
+          &nbsp;
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <MatchesList showHeadline={true} status={2} />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <MatchesList showHeadline={true} status={0} />
+        </Grid.Column>
+      </Grid.Row>
+    </MatchesContext.Provider>
+  );
+}
+
+export default MatchLists;
