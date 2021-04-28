a3c57099c6dcc5382481a7ce0992b625aaaf5531
olen-d <olen.daelhousen@outlook.com>
2021-04-28 01:59:46 -0400

    Refactor state input to use a dropdown list

--- src/components/formFields/stateInput.js
+++ src/components/formFields/stateInput.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-import { Form, Popup } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 const StateInput = props => {
   const { errors, handleBlur, handleChange, placeholder, values } = props;
+
+  const [states, setStates] = useState([]);
 
   const validate = event => {
     const { target: { name }, } = event;
@@ -12,24 +14,40 @@ const StateInput = props => {
     return values[name] && values[name].length > 1 ? false : true;  // Short circuit to avoid error when attempting to read length of undefined
   }
 
-  return(
-    <Popup
-      trigger={
-        <Form.Input
-          className="fluid"
-          icon="map pin"
-          iconPosition="left"
-          name="state"
-          value={values.state || ""}
-          placeholder={placeholder}
-          error={errors.state}
-          onBlur={(event) => handleBlur(validate(event), event)}
-          onChange={handleChange}
-        />
+  useEffect(()=> {
+    (async ()=> {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/states`);
+
+      const responseJson = response.status === 200 ? await response.json() : false;
+
+      if (responseJson && responseJson.status === 200) {
+        const { data } = responseJson;
+        setStates([...data]);
+      } else {
+        // TODO: Deal with the error 
       }
-      content="Please enter the name of your state."
-      on="focus"
-    />
+    })();
+  }, []);
+
+  return(
+    <>
+      <Form.Input
+        autoComplete="new-user-state"
+        className="fluid"
+        error={errors.state}
+        icon="map pin"
+        iconPosition="left"
+        list="states"
+        name="state"
+        onBlur={(event) => handleBlur(validate(event), event)}
+        onChange={handleChange}
+        placeholder={placeholder}
+        value={values.state || ""}
+      />
+      <datalist id="states">
+        {states.map(({ code, name }) => { return <option key={code} value={name} /> })}
+      </datalist>
+    </>
   );
 }
 
