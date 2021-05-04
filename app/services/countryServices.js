08b7ee770726198d118f419e3b99bda99a8b4829
olen-d <olen.daelhousen@outlook.com>
2021-05-03 22:16:13 -0400

    Refactor Countries Controller and split services into Country Services

--- app/services/countryServices.js
+++ app/services/countryServices.js
@@ -6,7 +6,7 @@ const logger = require("../utilities/logger");
 
 /**
  * @author Olen Daelhousen <hello@olen.dev>
- * @returns {object} - All country records with two and three letter alphabetical country codes, numeric country code, and country name
+ * @returns {object} - All country records with two and three letter alphabetical country codes, numeric country code, and country name.
  */
 
 exports.read_all_countries = async () => {
@@ -20,5 +20,26 @@ exports.read_all_countries = async () => {
   } catch (error) {
     logger.error(`server.service.country.read.all.countries ${error}`);
     throw new Error("Could not read all countries.");
+  }
+};
+
+/**
+ * @author Olen Daelhousen <hello@olen.dev>
+ * @param {string} alphatwo - ISO two letter country code
+ * @returns {object} - Country record that matches the ISO two letter country code provided. 
+ */
+
+exports.read_one_country_by_alpha_two = async alphatwo => {
+  try {
+    const data = await Country.findOne({
+      where: {
+        "alpha2": alphatwo
+      }
+    });
+
+    return data;
+  } catch (error) {
+    logger.error(`server.service.country.read.one.country.by.alpha.two ${alphatwo} ${error}`);
+    throw new Error(`Could not read country ${alphatwo}.`);
   }
 };
