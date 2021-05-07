82cba2224366398981c5bc5c5314224ecc7a961e
olen-d <olen.daelhousen@outlook.com>
2021-05-06 23:52:51 -0400

    Fix issue where app crashes when the state or country code is blank

--- app/routes/countries.js
+++ app/routes/countries.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const countries_controller = require("../controllers/countriesController");
 
 router.get("/countries", countries_controller.read_all_countries);
-router.get("/countries/alphatwo/:alphatwo", countries_controller.read_one_country_by_alpha_two);
+router.get("/countries/alphatwo/:alphatwo?", countries_controller.read_one_country_by_alpha_two);
 
 module.exports = router;
--- app/routes/states.js
+++ app/routes/states.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const states_controller = require("../controllers/statesController");
 
 router.get("/states", states_controller.read_all_states);
-router.get("/states/code/:code", states_controller.read_one_state_by_code);
+router.get("/states/code/:code?", states_controller.read_one_state_by_code);
 
 module.exports = router;
